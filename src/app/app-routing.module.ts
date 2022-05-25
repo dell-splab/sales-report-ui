c16ce5b80f767f0cbb98425c0d4e55971db7a409
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2022-05-25 13:51:35 -0300

    Create a table to list clients

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientComponent } from './client/client.component';
 import { HomeComponent } from './home/home.component';
 import { LeadsComponent } from './leads/leads.component';
 
@@ -8,6 +9,10 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'clients',
+    component: ClientComponent,
   },
   {
     path: 'leads',
