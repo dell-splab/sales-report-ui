3cc67fa0c4e6df086f8535b6b0ac4da6b5b2242c
Gabriela Motta <gabimotta15@gmail.com>
2022-06-03 11:03:40 -0300

    remove console

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -6,28 +6,106 @@ import { Client } from './client.model';
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
-  styleUrls: ['./client.component.scss']
+  styleUrls: ['./client.component.scss'],
 })
 export class ClientComponent implements OnInit {
-
   itemsSubject: Observable<any> = new Subject<any>();
   items: any[] = [
-    { name: 'Tyler Kennedy', email: 'kennedy.t@gmail.com', totalSpend: 'R$2.528,00', numberPurchases:'2', lastPurchaseData: '25/04/2022' },
-    { name: 'Tyler Kennedy', email: 'kennedy.t@gmail.com', totalSpend: 'R$2.528,00', numberPurchases:'1', lastPurchaseData: '12/08/2021' },
-    { name: 'Tyler Kennedy', email: 'kennedy.t@gmail.com', totalSpend: 'R$2.528,00', numberPurchases:'3', lastPurchaseData: '03/07/2021' },
-    { name: 'Tyler Kennedy', email: 'kennedy.t@gmail.com', totalSpend: 'R$2.528,00', numberPurchases:'5', lastPurchaseData: '15/08/2021' },
-    { name: 'Tyler Kennedy', email: 'kennedy.t@gmail.com', totalSpend: 'R$2.528,00', numberPurchases:'4', lastPurchaseData: '07/11/2021' },
-    { name: 'Tyler Kennedy', email: 'kennedy.t@gmail.com', totalSpend: 'R$2.528,00', numberPurchases:'3', lastPurchaseData: '07/08/2021' },
-    { name: 'Tyler Kennedy', email: 'kennedy.t@gmail.com', totalSpend: 'R$2.528,00', numberPurchases:'1', lastPurchaseData: '05/08/2021' },
-    { name: 'Tyler Kennedy', email: 'kennedy.t@gmail.com', totalSpend: 'R$2.528,00', numberPurchases:'5', lastPurchaseData: '01/02/2021' },
-    { name: 'Tyler Kennedy', email: 'kennedy.t@gmail.com', totalSpend: 'R$2.528,00', numberPurchases:'8', lastPurchaseData: '03/03/2021' },
-    { name: 'Tyler Kennedy', email: 'kennedy.t@gmail.com', totalSpend: 'R$2.528,00', numberPurchases:'1', lastPurchaseData: '04/06/2021' },
-    { name: 'Tyler Kennedy', email: 'kennedy.t@gmail.com', totalSpend: 'R$2.528,00', numberPurchases:'7', lastPurchaseData: '02/04/2021' },
-    { name: 'Tyler Kennedy', email: 'kennedy.t@gmail.com', totalSpend: 'R$2.528,00', numberPurchases:'1', lastPurchaseData: '09/08/2021' },
+    {
+      name: 'Tyler Kennedy',
+      email: 'kennedy.t@gmail.com',
+      totalSpend: 'R$2.528,00',
+      numberPurchases: '2',
+      lastPurchaseData: '25/04/2022',
+    },
+    {
+      name: 'Tyler Kennedy',
+      email: 'kennedy.t@gmail.com',
+      totalSpend: 'R$2.528,00',
+      numberPurchases: '1',
+      lastPurchaseData: '12/08/2021',
+    },
+    {
+      name: 'Tyler Kennedy',
+      email: 'kennedy.t@gmail.com',
+      totalSpend: 'R$2.528,00',
+      numberPurchases: '3',
+      lastPurchaseData: '03/07/2021',
+    },
+    {
+      name: 'Tyler Kennedy',
+      email: 'kennedy.t@gmail.com',
+      totalSpend: 'R$2.528,00',
+      numberPurchases: '5',
+      lastPurchaseData: '15/08/2021',
+    },
+    {
+      name: 'Tyler Kennedy',
+      email: 'kennedy.t@gmail.com',
+      totalSpend: 'R$2.528,00',
+      numberPurchases: '4',
+      lastPurchaseData: '07/11/2021',
+    },
+    {
+      name: 'Tyler Kennedy',
+      email: 'kennedy.t@gmail.com',
+      totalSpend: 'R$2.528,00',
+      numberPurchases: '3',
+      lastPurchaseData: '07/08/2021',
+    },
+    {
+      name: 'Tyler Kennedy',
+      email: 'kennedy.t@gmail.com',
+      totalSpend: 'R$2.528,00',
+      numberPurchases: '1',
+      lastPurchaseData: '05/08/2021',
+    },
+    {
+      name: 'Tyler Kennedy',
+      email: 'kennedy.t@gmail.com',
+      totalSpend: 'R$2.528,00',
+      numberPurchases: '5',
+      lastPurchaseData: '01/02/2021',
+    },
+    {
+      name: 'Tyler Kennedy',
+      email: 'kennedy.t@gmail.com',
+      totalSpend: 'R$2.528,00',
+      numberPurchases: '8',
+      lastPurchaseData: '03/03/2021',
+    },
+    {
+      name: 'Tyler Kennedy',
+      email: 'kennedy.t@gmail.com',
+      totalSpend: 'R$2.528,00',
+      numberPurchases: '1',
+      lastPurchaseData: '04/06/2021',
+    },
+    {
+      name: 'Tyler Kennedy',
+      email: 'kennedy.t@gmail.com',
+      totalSpend: 'R$2.528,00',
+      numberPurchases: '7',
+      lastPurchaseData: '02/04/2021',
+    },
+    {
+      name: 'Tyler Kennedy',
+      email: 'kennedy.t@gmail.com',
+      totalSpend: 'R$2.528,00',
+      numberPurchases: '1',
+      lastPurchaseData: '09/08/2021',
+    },
   ];
 
   filteredItems: any[] = [];
-  headers = ['Name', 'Email', 'Total spent', 'Number of purchases', 'Last purchase date', 'Action'];
+  headers = [
+    'Name',
+    'Email',
+    'Total spent',
+    'Number of purchases',
+    'Last purchase date',
+    'Action',
+  ];
 
   fromItem = 0;
   toItem = 4;
@@ -36,19 +114,18 @@ export class ClientComponent implements OnInit {
   newClientStatus = false;
 
   newClient: Client = {
-    name: "",
-    email: ""
+    name: '',
+    email: '',
   };
 
   newClientNotValid = false;
   newClientIsValid = false;
   showOverlay = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     this.fillTable(this.fromItem, this.toItem);
-    
   }
 
   navigate(iconName: string) {
@@ -78,12 +155,9 @@ export class ClientComponent implements OnInit {
 
   saveClient() {
     this.newClientStatus = false;
-    if(this.newClient.name === "" || this.newClient.email === ""){
+    if (this.newClient.name === '' || this.newClient.email === '') {
       this.newClientNotValid = true;
-      console.log(this.newClientStatus)
-    }
-    else {
-      console.log(this.newClient)
+    } else {
       this.newClientIsValid = true;
     }
   }
@@ -93,7 +167,7 @@ export class ClientComponent implements OnInit {
     this.newClientNotValid = false;
     this.showOverlay = false;
   }
-  
+
   openCard() {
     this.showOverlay = true;
     this.newClientStatus = true;
@@ -103,5 +177,4 @@ export class ClientComponent implements OnInit {
     this.newClientStatus = false;
     this.showOverlay = false;
   }
-
 }
--- src/app/sales/sales.component.ts
+++ src/app/sales/sales.component.ts
@@ -6,71 +6,128 @@ import { Sale } from './sales.model';
 @Component({
   selector: 'app-sales',
   templateUrl: './sales.component.html',
-  styleUrls: ['./sales.component.scss']
+  styleUrls: ['./sales.component.scss'],
 })
 export class SalesComponent implements OnInit {
-
   SalesSubject: Observable<any> = new Subject<any>();
   sales: any[] = [
-    { name: 'Tyler Kennedy', saleName: 'OptiPlex 3090 Micro', purchaseDate: '5-11-2022' },
-    { name: 'Tyler Kennedy', saleName: 'OptiPlex 3090 Micro', purchaseDate: '5-11-2022' },
-    { name: 'Tyler Kennedy', saleName: 'OptiPlex 3090 Micro', purchaseDate: '5-11-2022' },
-    { name: 'Tyler Kennedy', saleName: 'OptiPlex 3090 Micro', purchaseDate: '5-11-2022' },
-    { name: 'Tyler Kennedy', saleName: 'OptiPlex 3090 Micro', purchaseDate: '5-11-2022' },
-    { name: 'Tyler Kennedy', saleName: 'OptiPlex 3090 Micro', purchaseDate: '5-11-2022' },
-    { name: 'Tyler Kennedy', saleName: 'OptiPlex 3090 Micro', purchaseDate: '5-11-2022' },
-    { name: 'Tyler Kennedy', saleName: 'OptiPlex 3090 Micro', purchaseDate: '5-11-2022' },
-    { name: 'Tyler Kennedy', saleName: 'OptiPlex 3090 Micro', purchaseDate: '5-11-2022' },
-    { name: 'Tyler Kennedy', saleName: 'OptiPlex 3090 Micro', purchaseDate: '5-11-2022' },
-    { name: 'Tyler Kennedy', saleName: 'OptiPlex 3090 Micro', purchaseDate: '5-11-2022' },
-    { name: 'Tyler Kennedy', saleName: 'OptiPlex 3090 Micro', purchaseDate: '5-11-2022' },
-    { name: 'Tyler Kennedy', saleName: 'OptiPlex 3090 Micro', purchaseDate: '5-11-2022' },
-    { name: 'Tyler Kennedy', saleName: 'OptiPlex 3090 Micro', purchaseDate: '5-11-2022' },
-    { name: 'Tyler Kennedy', saleName: 'OptiPlex 3090 Micro', purchaseDate: '5-11-2022' },
-  ]
+    {
+      name: 'Tyler Kennedy',
+      saleName: 'OptiPlex 3090 Micro',
+      purchaseDate: '5-11-2022',
+    },
+    {
+      name: 'Tyler Kennedy',
+      saleName: 'OptiPlex 3090 Micro',
+      purchaseDate: '5-11-2022',
+    },
+    {
+      name: 'Tyler Kennedy',
+      saleName: 'OptiPlex 3090 Micro',
+      purchaseDate: '5-11-2022',
+    },
+    {
+      name: 'Tyler Kennedy',
+      saleName: 'OptiPlex 3090 Micro',
+      purchaseDate: '5-11-2022',
+    },
+    {
+      name: 'Tyler Kennedy',
+      saleName: 'OptiPlex 3090 Micro',
+      purchaseDate: '5-11-2022',
+    },
+    {
+      name: 'Tyler Kennedy',
+      saleName: 'OptiPlex 3090 Micro',
+      purchaseDate: '5-11-2022',
+    },
+    {
+      name: 'Tyler Kennedy',
+      saleName: 'OptiPlex 3090 Micro',
+      purchaseDate: '5-11-2022',
+    },
+    {
+      name: 'Tyler Kennedy',
+      saleName: 'OptiPlex 3090 Micro',
+      purchaseDate: '5-11-2022',
+    },
+    {
+      name: 'Tyler Kennedy',
+      saleName: 'OptiPlex 3090 Micro',
+      purchaseDate: '5-11-2022',
+    },
+    {
+      name: 'Tyler Kennedy',
+      saleName: 'OptiPlex 3090 Micro',
+      purchaseDate: '5-11-2022',
+    },
+    {
+      name: 'Tyler Kennedy',
+      saleName: 'OptiPlex 3090 Micro',
+      purchaseDate: '5-11-2022',
+    },
+    {
+      name: 'Tyler Kennedy',
+      saleName: 'OptiPlex 3090 Micro',
+      purchaseDate: '5-11-2022',
+    },
+    {
+      name: 'Tyler Kennedy',
+      saleName: 'OptiPlex 3090 Micro',
+      purchaseDate: '5-11-2022',
+    },
+    {
+      name: 'Tyler Kennedy',
+      saleName: 'OptiPlex 3090 Micro',
+      purchaseDate: '5-11-2022',
+    },
+    {
+      name: 'Tyler Kennedy',
+      saleName: 'OptiPlex 3090 Micro',
+      purchaseDate: '5-11-2022',
+    },
+  ];
 
   filteredSales: any[] = [];
-  headers = ['Name', "Product's name", 'Purchase date']
+  headers = ['Name', "Product's name", 'Purchase date'];
 
   fromSale = 0;
   toSale = 4;
   currentPage = 0;
   pageSize = 5;
-  
+
   newSaleStatus = false;
-  
+
   newSale: Sale = {
-    item: "",
-    email: ""
-  }
-  
+    item: '',
+    email: '',
+  };
+
   newSaleNotValid = false;
   newSaleIsValid = false;
 
   showOverlay = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.fillTable(this.fromSale, this.toSale)
+    this.fillTable(this.fromSale, this.toSale);
   }
-
 
   navigate(iconName: string) {
     this.router.navigate(['/', iconName]);
   }
-  
+
   fillTable(fromSale: number, toSale: number) {
     this.fromSale = fromSale;
 
     this.toSale = toSale;
 
     this.filteredSales = this.sales.slice(fromSale, toSale + 1);
-
   }
 
   changePage(event: any) {
-    this.fillTable(event.from, event.to)
+    this.fillTable(event.from, event.to);
   }
 
   changeItemsPerPage(event: any) {
@@ -84,12 +141,9 @@ export class SalesComponent implements OnInit {
 
   saveSale() {
     this.newSaleStatus = false;
-    if(this.newSale.item === "" || this.newSale.email === ""){
+    if (this.newSale.item === '' || this.newSale.email === '') {
       this.newSaleNotValid = true;
-      console.log(this.newSaleStatus)
-    }
-    else {
-      console.log(this.newSale)
+    } else {
       this.newSaleIsValid = true;
     }
   }
@@ -97,9 +151,9 @@ export class SalesComponent implements OnInit {
   closeNotification() {
     this.newSaleIsValid = false;
     this.newSaleNotValid = false;
-    this.showOverlay = true;
+    this.showOverlay = false;
   }
-  
+
   openCard() {
     this.showOverlay = true;
     this.newSaleStatus = true;
@@ -109,6 +163,4 @@ export class SalesComponent implements OnInit {
     this.newSaleStatus = false;
     this.showOverlay = false;
   }
-
-
 }
