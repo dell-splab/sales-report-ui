3b2203dde06ef786eb7234b673e83a2f89b23c3c
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2022-06-04 16:54:02 -0300

    create client

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -13,92 +13,6 @@ export class ClientComponent implements OnInit {
   itemsSubject: Observable<any> = new Subject<any>();
 
   clients: Client[] = [];
-  items: any[] = [
-    {
-      name: 'Tyler Kennedy',
-      email: 'kennedy.t@gmail.com',
-      totalSpend: 'R$2.528,00',
-      numberPurchases: '2',
-      lastPurchaseData: '25/04/2022',
-    },
-    {
-      name: 'Tyler Kennedy',
-      email: 'kennedy.t@gmail.com',
-      totalSpend: 'R$2.528,00',
-      numberPurchases: '1',
-      lastPurchaseData: '12/08/2021',
-    },
-    {
-      name: 'Tyler Kennedy',
-      email: 'kennedy.t@gmail.com',
-      totalSpend: 'R$2.528,00',
-      numberPurchases: '3',
-      lastPurchaseData: '03/07/2021',
-    },
-    {
-      name: 'Tyler Kennedy',
-      email: 'kennedy.t@gmail.com',
-      totalSpend: 'R$2.528,00',
-      numberPurchases: '5',
-      lastPurchaseData: '15/08/2021',
-    },
-    {
-      name: 'Tyler Kennedy',
-      email: 'kennedy.t@gmail.com',
-      totalSpend: 'R$2.528,00',
-      numberPurchases: '4',
-      lastPurchaseData: '07/11/2021',
-    },
-    {
-      name: 'Tyler Kennedy',
-      email: 'kennedy.t@gmail.com',
-      totalSpend: 'R$2.528,00',
-      numberPurchases: '3',
-      lastPurchaseData: '07/08/2021',
-    },
-    {
-      name: 'Tyler Kennedy',
-      email: 'kennedy.t@gmail.com',
-      totalSpend: 'R$2.528,00',
-      numberPurchases: '1',
-      lastPurchaseData: '05/08/2021',
-    },
-    {
-      name: 'Tyler Kennedy',
-      email: 'kennedy.t@gmail.com',
-      totalSpend: 'R$2.528,00',
-      numberPurchases: '5',
-      lastPurchaseData: '01/02/2021',
-    },
-    {
-      name: 'Tyler Kennedy',
-      email: 'kennedy.t@gmail.com',
-      totalSpend: 'R$2.528,00',
-      numberPurchases: '8',
-      lastPurchaseData: '03/03/2021',
-    },
-    {
-      name: 'Tyler Kennedy',
-      email: 'kennedy.t@gmail.com',
-      totalSpend: 'R$2.528,00',
-      numberPurchases: '1',
-      lastPurchaseData: '04/06/2021',
-    },
-    {
-      name: 'Tyler Kennedy',
-      email: 'kennedy.t@gmail.com',
-      totalSpend: 'R$2.528,00',
-      numberPurchases: '7',
-      lastPurchaseData: '02/04/2021',
-    },
-    {
-      name: 'Tyler Kennedy',
-      email: 'kennedy.t@gmail.com',
-      totalSpend: 'R$2.528,00',
-      numberPurchases: '1',
-      lastPurchaseData: '09/08/2021',
-    },
-  ];
 
   filteredItems: any[] = [];
   headers = [
@@ -164,6 +78,17 @@ export class ClientComponent implements OnInit {
       this.newClientNotValid = true;
     } else {
       this.newClientIsValid = true;
+      this.clientService.create(this.newClient).subscribe(() => {
+        this.clientService.read().subscribe((clients) => {
+          this.clients = clients;
+          this.fillTable(this.fromItem, this.toItem);
+        });
+        this.closeCard()
+      })
+      this.newClient = {
+        name: '',
+        email: '',
+      }
     }
   }
 
