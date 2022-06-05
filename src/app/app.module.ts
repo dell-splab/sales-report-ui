50624d2c44d72ce2c082d1b567fe62a409cb4ace
Gabriela <gabriela.motta@splab.ufcg.edu.br>
2022-06-05 19:40:00 -0300

    no errors schema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -33,5 +33,6 @@ import { FormsModule } from '@angular/forms';
   ],
   providers: [ThemeService],
   bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule {}
